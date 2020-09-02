ab05d93d86dc9f559b2b54eaffd3831688e15614
Dmitry <dzsh0619@netcracker.com>
2020-09-02 18:44:38 +0300

    Create DataSource.java

---
+++ src/main/java/db/DataSource.java
@@ -0,0 +1,26 @@
+package db;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl( "jdbc:postgresql://localhost/bookup" );
+        config.setUsername( "bookupAdmin" );
+        config.setPassword( "bookupAdmin" );
+        ds = new HikariDataSource( config );
+    }
+
+    private DataSource() {}
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
